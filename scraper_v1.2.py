36db81ac3ce99facf4837117d4206e3996df2cb8
Cole MacLean <maclean.cole@gmail.com>
2016-10-20 13:53:41 +0200

    --refactor --selenium refactored into dict structure instead of object, initial prototype working (able to pull and store data)

--- scraper_v1.2.py
+++ scraper_v1.2.py
@@ -1,6 +1,5 @@
 import time
 import json
-import Review
 from bs4 import BeautifulSoup
 import configparser
 from selenium import webdriver
@@ -17,25 +16,21 @@ username = config.get('Glassdoor', 'email')
 password = config.get('Glassdoor', 'password')
 
 # Manual options for the company, num pages to scrape, and URL
-pages = 10
+pages = 1
 companyName = "microsoft"
 companyURL = "https://www.glassdoor.com/Interview/Microsoft-Software-Development-Engineer-Interview-Questions-EI_IE1651.0,9_KO10,39.htm"
 
 def obj_dict(obj):
     return obj.__dict__
-#enddef
 
 def json_export(data):
-	jsonFile = open(companyName + ".json", "w")
-	jsonFile.write(json.dumps(data, indent=4, separators=(',', ': '), default=obj_dict))
-	jsonFile.close()
-#enddef
+    with open('interview_data.json', 'w') as outfile:
+        json.dump(data, outfile)
 
 def init_driver():
-    driver = webdriver.Chrome(executable_path = "./chromedriver")
+    driver = webdriver.Chrome(executable_path = "C:/chromedriver.exe")
     driver.wait = WebDriverWait(driver, 10)
     return driver
-#enddef
 
 def login(driver, username, password):
     driver.get("http://www.glassdoor.com/profile/login_input.htm")
@@ -52,45 +47,41 @@ def login(driver, username, password):
         login_button.click()
     except TimeoutException:
         print("TimeoutException! Username/password field or login button not found on glassdoor.com")
-#enddef
 
 def parse_reviews_HTML(reviews, data):
+	r = {'data':'','role':'','gotOffer':'','experience':'','difficulty':'','length':'','details':'','questions':''}
 	for review in reviews:
-		length = "-"
-		gotOffer = "-"
-		experience = "-"
-		difficulty = "-"
-		date = review.find("time", { "class" : "date" }).getText().strip()
-		role = review.find("span", { "class" : "reviewer"}).getText().strip()
+		r['date'] = review.find("time", { "class" : "date" }).getText().strip()
+		r['role'] = review.find("span", { "class" : "reviewer"}).getText().strip()
 		outcomes = review.find_all("div", { "class" : ["tightLt", "col"] })
 		if (len(outcomes) > 0):
-			gotOffer = outcomes[0].find("span", { "class" : "middle"}).getText().strip()
-		#endif
+			r['gotOffer'] = outcomes[0].find("span", { "class" : "middle"}).getText().strip()
+		
 		if (len(outcomes) > 1):
-			experience = outcomes[1].find("span", { "class" : "middle"}).getText().strip()
-		#endif
+			r['experience'] = outcomes[1].find("span", { "class" : "middle"}).getText().strip()
+		
 		if (len(outcomes) > 2):
-			difficulty = outcomes[2].find("span", { "class" : "middle"}).getText().strip()
-		#endif
+			r['difficulty'] = outcomes[2].find("span", { "class" : "middle"}).getText().strip()
+		
 		appDetails = review.find("p", { "class" : "applicationDetails"})
 		if (appDetails):
 			appDetails = appDetails.getText().strip()
 			tookFormat = appDetails.find("took ")
 			if (tookFormat >= 0):
 				start = appDetails.find("took ") + 5
-				length = appDetails[start :].split('.', 1)[0]
-			#endif
+				r['length'] = appDetails[start :].split('.', 1)[0]
+			
 		else:
 			appDetails = "-"
-		#endif
+		
 		details = review.find("p", { "class" : "interviewDetails"})
 		if (details):
 			s = details.find("span", { "class" : ["link", "moreLink"] })
 			if (s):
 				s.extract() # Remove the "Show More" text and link if it exists
-			#endif
+			
 			details = details.getText().strip()
-		#endif
+		
 		questions = []
 		qs = review.find_all("span", { "class" : "interviewQuestion"})
 		if (qs):
@@ -98,28 +89,24 @@ def parse_reviews_HTML(reviews, data):
 				s = q.find("span", { "class" : ["link", "moreLink"] })
 				if (s):
 					s.extract() # Remove the "Show More" text and link if it exists
-				#endif
-				questions.append(q.getText().encode('utf-8').strip())
-			#endfor
-		#endif
-		r = Review.Review(date, role, gotOffer, experience, difficulty, length, details, questions)
+				questions.append(q.getText().strip())
+		r['questions'] = questions
 		data.append(r)
-	#endfor
+	
 	return data
-#enddef
 
 def get_data(driver, URL, startPage, endPage, data, refresh):
 	if (startPage > endPage):
 		return data
-	#endif
+	
 	print ("\nPage " + str(startPage) + " of " + str(endPage))
 	currentURL = URL + "_IP" + str(startPage) + ".htm"
 	time.sleep(2)
-	#endif
+	
 	if (refresh):
 		driver.get(currentURL)
 		print ("Getting " + currentURL)
-	#endif
+	
 	time.sleep(2)
 	HTML = driver.page_source
 	soup = BeautifulSoup(HTML, "html.parser")
@@ -130,15 +117,14 @@ def get_data(driver, URL, startPage, endPage, data, refresh):
 		if (startPage % 10 == 0):
 			print ("\nTaking a breather for a few seconds ...")
 			time.sleep(10)
-		#endif
+		
 		get_data(driver, URL, startPage + 1, endPage, data, True)
 	else:
 		print ("Waiting ... page still loading or CAPTCHA input required")
 		time.sleep(3)
 		get_data(driver, URL, startPage, endPage, data, False)
-	#endif
+	
 	return data
-#enddef
 
 if __name__ == "__main__":
 	driver = init_driver()
@@ -148,7 +134,7 @@ if __name__ == "__main__":
 	time.sleep(5)
 	print ("\nStarting data scraping ...")
 	data = get_data(driver, companyURL[:-4], 1, pages, [], True)
-	print ("\nExporting data to " + companyName + ".json")
+	print ("\nExporting data")
 	json_export(data)
 	driver.quit()
-#endif
+
